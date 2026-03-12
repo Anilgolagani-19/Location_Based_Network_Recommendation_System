1204f3f39277fa914e34fe7dcb71902d56e030ea
Anil <anilgolagabi2011@gmail.com>
2026-03-13 01:42:26 +0530

    modified our-ai

--- backend/admin_api.py
+++ backend/admin_api.py
@@ -60,4 +60,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
